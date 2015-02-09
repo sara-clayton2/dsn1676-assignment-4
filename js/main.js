1b5e4adb739b310a655fe65eb39fefd8b9525afa
sara clayton <clay0037@algonquinlive.com>
2015-02-09 18:46:13 -0500

    named variables for each data
    listed three different things and labeled there variables

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var blueberries = { 
+    latin: 'Vaccinium corymbosum',
+    growsOnShrubs: 'true', 
+    energy: '240 kJ', 
+    carbohydrates: '14.49 g', 
+    protein: '0.74 g'
+}; 
+
+var grapes = { 
+    latin: 'Vitis vinifera',
+    growsOnShrubs: 'false', 
+    energy: '288 kJ', 
+    carbohydrates: '18.1 g', 
+    protein: '0.72 g'
+}; 
+
+var redcurrant = { 
+    latin: 'Ribes rubrum',
+    growsOnShrubs: 'true', 
+    energy: '234 kJ', 
+    carbohydrates: '13.8 g', 
+    protein: '1.4 g'
+}; 
